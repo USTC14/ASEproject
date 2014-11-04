2cfe1ea28c55726746dafe66fcd7581ea20e408a
yqmighty <499439965@qq.com>
2014-11-04 21:14:47 +0800

    the DB file

--- sds.sql
+++ sds.sql
@@ -1,3 +1,8 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.14 
+MySQL - 5.0.18-nt : Database - sds
+*********************************************************************
+*/
 /*!40101 SET NAMES utf8 */;
 
 /*!40101 SET SQL_MODE=''*/;
@@ -62,3 +67,65 @@ CREATE TABLE `auth_permission` (
 /*Data for the table `auth_permission` */
 
 insert  into `auth_permission`(`id`,`name`,`content_type_id`,`codename`) values (1,'Can add permission',1,'add_permission'),(2,'Can change permission',1,'change_permission'),(3,'Can delete permission',1,'delete_permission'),(4,'Can add group',2,'add_group'),(5,'Can change group',2,'change_group'),(6,'Can delete group',2,'delete_group'),(7,'Can add user',3,'add_user'),(8,'Can change user',3,'change_user'),(9,'Can delete user',3,'delete_user'),(10,'Can add content type',4,'add_contenttype'),(11,'Can change content type',4,'change_contenttype'),(12,'Can delete content type',4,'delete_contenttype'),(13,'Can add session',5,'add_session'),(14,'Can change session',5,'change_session'),(15,'Can delete session',5,'delete_session'),(16,'Can add site',6,'add_site'),(17,'Can change site',6,'change_site'),(18,'Can delete site',6,'delete_site'),(19,'Can add questions',7,'add_questions'),(20,'Can change questions',7,'change_questions'),(21,'Can delete questions',7,'delete_questions'),(22,'Can add result',8,'add_result'),(23,'Can change result',8,'change_result'),(24,'Can delete result',8,'delete_result'),(25,'Can add log entry',9,'add_logentry'),(26,'Can change log entry',9,'change_logentry'),(27,'Can delete log entry',9,'delete_logentry');
+
+/*Table structure for table `auth_user` */
+
+DROP TABLE IF EXISTS `auth_user`;
+
+CREATE TABLE `auth_user` (
+  `id` int(11) NOT NULL auto_increment,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime NOT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_user` */
+
+insert  into `auth_user`(`id`,`password`,`last_login`,`is_superuser`,`username`,`first_name`,`last_name`,`email`,`is_staff`,`is_active`,`date_joined`) values (1,'pbkdf2_sha256$10000$x20sjp6TzqrP$cyJOtDR2e80Zykekx9vF/TFcWi53PxeWHdyXasGgcas=','2014-05-31 06:56:13',1,'admin','','','ertongzixingche@163.com',1,1,'2014-05-02 05:22:07'),(2,'pbkdf2_sha256$10000$bHgTa5lHL488$gQPge67fIL7UaeahaGN2hOEuTg5lc81QV2jzm2O3lb8=','2014-05-15 13:22:32',0,'sb','É¯É¯','ÂÞ','',0,1,'2014-05-15 13:22:32'),(3,'pbkdf2_sha256$10000$wVl52MCvofNg$eHXTpSD0BqwWmshSrZC1xxaCOxgwSzIsBixi6SQf4kg=','2014-05-28 05:29:04',0,'jerry','ÁÖ','Â¶Â¶','',0,1,'2014-05-28 05:29:04'),(5,'pbkdf2_sha256$10000$zM7lK36t20D9$wmfKZRIea+028rrCHUz/1fJQ2c8B4YhIClslmeGikH0=','2014-05-31 02:30:35',0,'aaa','test','test','test@163.com',1,1,'2014-05-31 02:29:35');
+
+/*Table structure for table `auth_user_groups` */
+
+DROP TABLE IF EXISTS `auth_user_groups`;
+
+CREATE TABLE `auth_user_groups` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`group_id`),
+  KEY `auth_user_groups_6340c63c` (`user_id`),
+  KEY `auth_user_groups_5f412f9a` (`group_id`),
+  CONSTRAINT `group_id_refs_id_274b862c` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  CONSTRAINT `user_id_refs_id_40c41112` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_user_groups` */
+
+insert  into `auth_user_groups`(`id`,`user_id`,`group_id`) values (1,2,2);
+
+/*Table structure for table `auth_user_user_permissions` */
+
+DROP TABLE IF EXISTS `auth_user_user_permissions`;
+
+CREATE TABLE `auth_user_user_permissions` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`permission_id`),
+  KEY `auth_user_user_permissions_6340c63c` (`user_id`),
+  KEY `auth_user_user_permissions_83d7f98b` (`permission_id`),
+  CONSTRAINT `permission_id_refs_id_35d9ac25` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `user_id_refs_id_4dc23c39` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_user_user_permissions` */
