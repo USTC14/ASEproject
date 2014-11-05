75b8561f6a71a0dc767099004966634fff460681
yqmighty <499439965@qq.com>
2014-11-05 22:56:01 +0800

    the thirdth DB file

--- sds.sql
+++ sds.sql
@@ -129,3 +129,89 @@ CREATE TABLE `auth_user_user_permissions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `auth_user_user_permissions` */
+
+/*Table structure for table `django_admin_log` */
+
+DROP TABLE IF EXISTS `django_admin_log`;
+
+CREATE TABLE `django_admin_log` (
+  `id` int(11) NOT NULL auto_increment,
+  `action_time` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content_type_id` int(11) default NULL,
+  `object_id` longtext,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL,
+  `change_message` longtext NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `django_admin_log_6340c63c` (`user_id`),
+  KEY `django_admin_log_37ef4eb4` (`content_type_id`),
+  CONSTRAINT `content_type_id_refs_id_93d2d1f8` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `user_id_refs_id_c0d12874` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `django_admin_log` */
+
+insert  into `django_admin_log`(`id`,`action_time`,`user_id`,`content_type_id`,`object_id`,`object_repr`,`action_flag`,`change_message`) values (1,'2014-05-15 13:21:26',1,2,'1','普通用户',1,''),(2,'2014-05-15 13:21:58',1,2,'1','普通用户',3,''),(3,'2014-05-15 13:22:32',1,3,'2','sb',1,''),(4,'2014-05-15 13:23:35',1,3,'2','sb',2,'已修改 password，first_name 和 last_name 。'),(5,'2014-05-15 13:24:24',1,2,'2','普通用户组',1,''),(6,'2014-05-15 13:24:59',1,3,'2','sb',2,'已修改 password 和 groups 。'),(7,'2014-05-15 13:26:22',1,6,'2','http://127.0.0.1:8000/',1,''),(8,'2014-05-23 23:36:32',1,7,'1','Questions object',2,'已修改 content 。'),(9,'2014-05-23 23:37:45',1,7,'1','Questions object',2,'已修改 content 。'),(10,'2014-05-25 02:38:21',1,7,'60','Questions object',2,'已修改 content 。'),(11,'2014-05-25 11:30:29',1,7,'60','Questions object',2,'已修改 content 。'),(12,'2014-05-25 11:39:13',1,7,'60','Questions object',2,'已修改 content 。'),(13,'2014-05-25 11:45:37',1,7,'60','Questions object',2,'已修改 content 。'),(14,'2014-05-28 05:29:04',1,3,'3','jerry',1,''),(15,'2014-05-28 05:30:47',1,3,'3','jerry',2,'已修改 password，first_name 和 last_name 。'),(16,'2014-05-30 15:15:23',1,7,'1','Questions object',2,'已修改 content 。'),(17,'2014-05-30 15:15:49',1,7,'1','Questions object',2,'已修改 content 。'),(18,'2014-05-31 01:49:51',1,7,'60','Questions object',2,'已修改 content 。'),(19,'2014-05-31 02:16:59',1,3,'4','aaa',1,''),(20,'2014-05-31 02:29:10',1,3,'4','aaa',3,''),(21,'2014-05-31 02:29:36',1,3,'5','aaa',1,''),(22,'2014-05-31 02:30:16',1,3,'5','aaa',2,'已修改 password，first_name，last_name，email 和 is_staff 。'),(23,'2014-05-31 04:33:55',1,7,'60','Questions object',2,'已修改 content 。'),(24,'2014-05-31 06:56:27',1,7,'60','Questions object',2,'已修改 content 。');
+
+/*Table structure for table `django_content_type` */
+
+DROP TABLE IF EXISTS `django_content_type`;
+
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `app_label` (`app_label`,`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `django_content_type` */
+
+insert  into `django_content_type`(`id`,`name`,`app_label`,`model`) values (1,'permission','auth','permission'),(2,'group','auth','group'),(3,'user','auth','user'),(4,'content type','contenttypes','contenttype'),(5,'session','sessions','session'),(6,'site','sites','site'),(7,'questions','self_directed_search','questions'),(8,'result','self_directed_search','result'),(9,'log entry','admin','logentry');
+
+/*Table structure for table `django_session` */
+
+DROP TABLE IF EXISTS `django_session`;
+
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime NOT NULL,
+  PRIMARY KEY  (`session_key`),
+  KEY `django_session_b7b81f0c` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `django_session` */
+
+insert  into `django_session`(`session_key`,`session_data`,`expire_date`) values ('0anfmkll3xxagj690ehg3tbeb54ve34s','MWFhZmVmMTEzMzc5NzZhNjgxNTM1YTM1ZWM5NGRiNWY2ZGNmNWQ1Njp7Il9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9pZCI6MX0=','2014-05-30 06:58:34'),('kotbkrmronm1x7kl1cwi6ntocbnln628','MWFhZmVmMTEzMzc5NzZhNjgxNTM1YTM1ZWM5NGRiNWY2ZGNmNWQ1Njp7Il9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9pZCI6MX0=','2014-05-29 13:18:30'),('pzb310a4td203jzk5q6vzkkdgm7bnaf5','MWFhZmVmMTEzMzc5NzZhNjgxNTM1YTM1ZWM5NGRiNWY2ZGNmNWQ1Njp7Il9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9pZCI6MX0=','2014-06-14 06:56:13');
+
+/*Table structure for table `django_site` */
+
+DROP TABLE IF EXISTS `django_site`;
+
+CREATE TABLE `django_site` (
+  `id` int(11) NOT NULL auto_increment,
+  `domain` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `django_site` */
+
+insert  into `django_site`(`id`,`domain`,`name`) values (1,'example.com','example.com'),(2,'http://127.0.0.1:8000/','霍兰德问卷调查前端首页');
+
+/*Table structure for table `self_directed_search_questions` */
+
+DROP TABLE IF EXISTS `self_directed_search_questions`;
+
+CREATE TABLE `self_directed_search_questions` (
+  `id` int(11) NOT NULL auto_increment,
+  `type` varchar(30) NOT NULL,
+  `content` varchar(100) NOT NULL,
+  `name` varchar(20) default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `self_directed_search_questions` */
