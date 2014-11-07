59a55c105c7b6c2b046964ed661ea81e1974ac36
ZhipengG <beyond_acm@163.com>
2014-11-07 21:27:18 +0800

    add a new file
    add a question.js file

---
+++ self_directed_search/static/js/question.js
@@ -0,0 +1,22 @@
+﻿$(function($) {
+	// 随机选取radio
+	$("#randChoose").bind("click", function() {
+		var $radios = $(":radio");
+		var count = 0;
+		var num = 0;
+		for (var i = 0; i < $radios.length / 2; i++) {
+			count = i * 2;
+			if (Math.random() < 0.5) {
+				num = count;
+			} else {
+				num = count + 1;
+			}
+			$(":radio").eq(num).attr("checked", "checked");
+		}
+	});
+	// 设置所有 radio 为未被选中
+	$("#removeChoose").bind("click", function() {
+		$(":radio").removeAttr("checked");
+	});
+});
+
