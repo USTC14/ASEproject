007fd326ffeda0fa6493bdb8dbddae3086d89b34
ZhipengG <beyond_acm@163.com>
2014-11-08 11:15:19 +0800

    modified the question.js file

--- self_directed_search/static/js/question.js
+++ self_directed_search/static/js/question.js
@@ -1,4 +1,4 @@
-﻿$(function($) {
+$(function($) {
 	// 随机选取radio
 	$("#randChoose").bind("click", function() {
 		var $radios = $(":radio");
@@ -20,3 +20,17 @@
 	});
 });
 
+/*
+ * function linkTo(y) {
+ * 
+ * window.location.hash = y; }
+ * 
+ * function form_check() { var a = document.getElementsByTagName("INPUT"); var b =
+ * document.getElementsByTagName("STRONG"); for (var i = 0; i < a.length; i++) {
+ * var tmpStr; var j = i / 2; tmpStr = a[i].name; var strongid = b[j]; var
+ * tmpjuanbiao = tmpStr + "juanbiao"; if (a[i].type == "radio" &&
+ * eval("document.form1." + tmpStr + "[0].checked") == false &&
+ * eval("document.form1." + tmpStr + "[1].checked") == false) { alert("问题'" +
+ * tmpStr + "'没有选择"); linkTo(tmpjuanbiao); strongid.style.color = "#ff0000";
+ * return false; } } }
+ */
